8984ad8756e884b7c41419d24ae5bf1f8a7ce625
Shreya <146594221+shjangada@users.noreply.github.com>
2024-08-21 09:43:45 -0400

    Update MainPage.js
    Changed mini icons on main page

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,7 +4,7 @@ import './supporting/style/MainPage.css'; // for MainPage.js
 import './supporting/style/Footer.css'; // for Footer.js
 import NavBar from './NavBar'; // Import the NavBar component
 import Footer from './Footer'
-import { ListChecks, RocketLaunch, Target } from '@phosphor-icons/react'; // Import Phosphor Icons
+import { Compass, RocketLaunch, Target } from '@phosphor-icons/react'; // Import Phosphor Icons
 
 
 const Header = () => {
@@ -40,17 +40,17 @@ const FoundationInfo = () => (
 const HeaderBoxes = () => (
   <div className="info-container">
     <div className="info-box">
-      <ListChecks size={75} color="#45613D" weight="fill" className="team-img"/>
+      <<Compass size={75} color="#45613D" weight="duotone" className="team-img"/>
       <p className="info-title">MISSION</p>
       <p className="info-p">The Vasant Shobha Foundation was established with the mission to provide quality education to people of all ages. We believe in the power of education to transform lives and communities, fostering opportunities for growth, empowerment, and societal advancement. Through our diverse programs and initiatives, we strive to create a world where knowledge is accessible to all, enabling individuals to reach their full potential and contribute meaningfully to society.</p>
     </div>
     <div className="info-box">
-      <RocketLaunch size={75} color="#45613D" weight="fill" className="team-img"/>
+      <RocketLaunch size={75} color="#45613D" weight="duotone" className="team-img"/>
       <p className="info-title">VISION</p>
       <p className="info-p">Our dedicated team of educators and volunteers, many of whom hail from similar backgrounds as those we serve, bring a unique perspective and deep understanding of the challenges faced by our community members. They serve as mentors and role models, inspiring students to reach their full potential despite any obstacles they may encounter. This shared connection creates a supportive environment where individuals feel valued and empowered to pursue their educational goals.</p>
     </div>
     <div className="info-box">
-      <Target size={75} color="#45613D" weight="fill" className="team-img"/>
+      <Target size={75} color="#45613D" weight="duotone" className="team-img"/>
       <p className="info-title">ACTION</p>
       <p className="info-p">We partner with local schools, community centers, and other organizations to offer a variety of educational opportunities. Together, we develop innovative initiatives tailored to the specific needs of each group, fostering a culture of lifelong learning and empowerment. Additionally, these partnerships enable us to leverage resources and expertise, maximizing the impact of our educational efforts and creating lasting change in the communities we serve.</p>
     </div>
